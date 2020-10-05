14098af67a6b1776e1736d0ce599de282b3e379c
David Hambraeus <Ddhambraeus@gmail.com>
2020-10-05 16:20:47 +0200

    fixes

--- hydrogen_wavefcns.py
+++ hydrogen_wavefcns.py
@@ -10,7 +10,7 @@ from spherical_harmonics import Ylm
 # The convention used is PHI for azimuthal angle and THETA for polar.
 def psi_nlm(n, l, m, R, THETA, PHI):
     a = 0.529e-10 # m - bohrs constant
-    psi = np.sqrt((2 / (n * a))**3 * np.prod(np.arange(n+l, n-l-1, -1)) / (2*n)) \
+    psi = np.sqrt((2 / (n * a))**3 / (np.prod(np.arange(n+l, n-l-1, -1)) * (2*n)) \
         * np.exp(-R/(n*a)) * (2 * R/ (n*a))**l \
         * sp.assoc_laguerre(2*R/(n*a), n-l-1, 2*l+1) \
         * Ylm(l, m, THETA, PHI)
